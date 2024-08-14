62ff193af6f379f9e7c9b14ac477ceb597469d5d
EmreSpo <emre.salihpasaogluTR@gmail.com>
2024-08-14 13:37:39 +0300

    Update Program.cs

--- GetAPI - .NET/Program.cs
+++ GetAPI - .NET/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient(); // HttpClient'ı ekle
+builder.Services.AddMemoryCache(); // MemoryCache'i ekle
 
 var app = builder.Build();
 
@@ -13,13 +14,12 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
-// /api/users dizinine yönlendirme
+// /api/users dizinine yönlendir
 app.MapGet("/", (HttpContext context) =>
 {
     context.Response.Redirect("/api/users");
     return Task.CompletedTask;
 });
 app.MapControllers();
-
 
 app.Run("https://localhost:7060/");
